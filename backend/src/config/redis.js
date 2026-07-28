b725ad29c666bef5bf56f4549cfd7f40b28f5698
Devkant <devkantsah43@gmail.com>
2026-07-28 21:08:30 +0530

    deployment procedure going on...

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,12 +1,7 @@
 const { createClient } = require("redis");
 
 const RedisClient = createClient({
-    username: 'default',
-    password: process.env.REDIS_HOST_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST || 'localhost',
-        port: 12132
-    }
+    url: process.env.REDIS_CONNECTION_STRING,
 });
 async function connectRedis() {
     try {
@@ -14,7 +9,7 @@ async function connectRedis() {
         console.log("Redis connected successfully");
     } catch (err) {
         console.error("Redis connection error:", err);
-        process.exit(1); // Exit process with failure
+        // process.exit(1); // Exit process with failure
     }
 }
 
