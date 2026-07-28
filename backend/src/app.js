c79c7f9599041848cc37e2c22c2f1e3bf05d7db9
Devkant <devkantsah43@gmail.com>
2026-07-28 21:24:47 +0530

    deployment procedure going on...

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser"); //now we can access cookies via r
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: "https://sahayak-kappa-umber.vercel.app", // Replace with your frontend URL
     credentials: true, // Allow cookies to be sent
 })
 )
