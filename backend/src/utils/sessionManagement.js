270008e6c46d81c220107cea4488bd256fe6a010
Devkant <devkantsah43@gmail.com>
2026-07-28 22:37:31 +0530

    deployment procedure going on...

--- backend/src/utils/sessionManagement.js
+++ backend/src/utils/sessionManagement.js
@@ -32,7 +32,7 @@ const sessionManagement = async (res, statusCode, user) => {
     res.cookie("refreshToken", RefreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000
     });
 
